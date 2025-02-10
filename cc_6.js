8e62b7f7a29fe62df46f9d42e488a69be47d1c9a
danielaj2004 <danielajoya12@gmail.com>
2025-02-09 21:55:41 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -15,3 +15,21 @@ const calculateSalesTax = function(amount, taxRate) {
 
 console.log(calculateSalesTax(100, 0.07)); // Sales is $7
 console.log(calculateSalesTax(500, 0.1)); // Sales is $50
+
+// Task 3: Arrow Function
+const calculateBonus = (salary, performanceRating) => {
+    let bonus;
+    if (performanceRating === "Average") {
+        bonus = salary * 0.05;
+    }
+    else if (performanceRating === "Good") {
+        bonus = salary * 0.1;
+    }
+    else if (performanceRating === "Excellent") {
+        bonus = salary * 0.2;
+    }
+    return `Bonus: $${bonus}`;
+}; // function to calculate bonuses
+
+console.log(calculateBonus(5000, "Excellent")); // Bonus is $1000
+console.log(calculateBonus(7000, "Good")); // Bonus is $700
