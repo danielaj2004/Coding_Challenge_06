782c51aa24d67c71da9e35e028f3a8f3114fd56c
danielaj2004 <danielajoya12@gmail.com>
2025-02-09 21:38:39 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -6,3 +6,12 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 
 console.log(calculateProfit(20, 30, 100)); // profit is $1000
 console.log(calculateProfit(50, 70, 200)); // profit is $4000
+
+// Task 2: Function Expression
+const calculateSalesTax = function(amount, taxRate) {
+    let salesTax = Math.floor(amount * taxRate)
+    return `Sales Tax: $${salesTax}`;
+}; // function to find sales tax
+
+console.log(calculateSalesTax(100, 0.07)); // Sales is $7
+console.log(calculateSalesTax(500, 0.1)); // Sales is $50
